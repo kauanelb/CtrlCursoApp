d662036e0574f091013469b47ffddd38f45196cf
kauanelb <kauane.lb@aluno.ifsc.edu.br>
2025-11-12 17:18:09 -0300

    v1.20251112

--- app/src/main/java/br/edu/ifsc/aluno/kauanelb/mobile/ctrlcurso/model/Aluno.java
+++ app/src/main/java/br/edu/ifsc/aluno/kauanelb/mobile/ctrlcurso/model/Aluno.java
@@ -1,5 +1,10 @@
 package br.edu.ifsc.aluno.kauanelb.mobile.ctrlcurso.model;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,14 +12,56 @@ import java.util.Date;
 public class Aluno {
 
     //atributos
+    private int matricula;
     private String nome;
-    private String curso;
     private String dtMatricula;
     private String dtNascimento;
     private String telefone;
     private int idCurso;
 
+    //construtores
+    public Aluno(){
+        this.matricula = 0 ;
+        this.nome = "";
+        this.dtMatricula = "1900-01-01";
+        this.dtNascimento = "1900-01-01";
+        this.telefone = "";
+        this.idCurso = 0;
+    }
+
+    //CONSTRUTOR - inicializa atributo de um arquivo JSon, recebe json
+    public Aluno(JSONObject jp){
+        try {
+            this.setMatricula(jp.getInt("nrMatricula"));
+            this.setNome(jp.getString("nmAluno"));
+            this.setDtMatricula(jp.getString("dtMatricula"));
+            this.setDtNascimento(jp.getString("dtNascimento"));
+            this.setTelefone(jp.getString("nrTelefone"));
+            this.setIdCurso(jp.getInt("idCursoCadastro"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Metodo retorna o objeto com dados no formato JSON
+    public JSONObject toJsonObject() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("nrMatricula", this.matricula);
+            json.put("nmAluno", this.nome);
+            json.put("dtMatricula", this.dtMatricula);
+            json.put("dtNascimento", this.dtNascimento);
+            json.put("nrTelefone", this.telefone);
+            json.put("idCursoCadastro", this.idCurso);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+
     //métodos
+
     public String getNome() {
         return nome;
     }
@@ -27,12 +74,12 @@ public class Aluno {
         }
     }
 
-    public String getCurso() {
-        return curso;
+    public int getMatricula() {
+        return matricula;
     }
 
-    public void setCurso(String curso) {
-        this.curso = curso;
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
     }
 
     public String getDtMatricula() {
